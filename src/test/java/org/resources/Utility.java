30c225306849366b7401b74b2fc472a38842e93d
venkatesh30-sm <venkateshsmtester@gmail.com>
2020-06-04 13:25:36 +0530

    m

--- src/test/java/org/resources/Utility.java
+++ src/test/java/org/resources/Utility.java
@@ -1,5 +1,18 @@
 package org.resources;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.stepdefinition.Hooks;
@@ -36,6 +49,50 @@ public class Utility {
 		js.executeScript("arguments[0].click()", w);
 	}
 	
+	
+	public static List<LinkedHashMap<String,String>> readDatas() throws Exception {
+		
+		List<LinkedHashMap<String,String>>listMap=new ArrayList<LinkedHashMap<String,String>>();
+		File f=new File(System.getProperty("user.dir")+"//target//excel//Cucumber-guru-data.xlsx");
+	
+        FileInputStream stream=new FileInputStream(f);
+        
+        Workbook w=new XSSFWorkbook(stream);
+        
+        Sheet s = w.getSheet("Sheet1");
+        Row headerRow = s.getRow(0);
+        
+        for(int i=1;i<s.getPhysicalNumberOfRows();i++) {
+        	
+        	Row eachRow = s.getRow(i);
+        	//Map
+        	LinkedHashMap <String,String> mp=new LinkedHashMap<String,String>();
+        	
+        	for(int j=0;j<headerRow.getPhysicalNumberOfCells();j++) {
+        		
+        		Cell eachCell = eachRow.getCell(j);
+        		int type = eachCell.getCellType();
+        		
+        		if(type==1) {
+     
+        	mp.put(headerRow.getCell(j).getStringCellValue(),eachCell.getStringCellValue() );
+        		}
+        		else {
+        			long l=(long)eachCell.getNumericCellValue();
+        			
+        			mp.put(headerRow.getCell(j).getStringCellValue(), String.valueOf(l));
+        		}
+        	}
+        	
+        	listMap.add(mp);
+        	
+        }
+		return listMap;
+        
+	
+		
+	
+	}
 	
 
 }
